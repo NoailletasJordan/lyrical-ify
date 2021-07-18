bb7d167b2479f203524c1389bb031e6771dbd616
NoailletasJordan <j.noailletas@gmail.com>
2021-07-18 19:02:44 +0200

    fixed a breaking bug by changing the special google link by the classic one

--- backend/child-window.js
+++ backend/child-window.js
@@ -2,42 +2,42 @@ const path = require("path")
 const { BrowserWindow } = require("electron")
 
 module.exports = () => {
-  let childWindow = new BrowserWindow({
-    parent: mainWindow,
-    show: false,
-    webPreferences: {
-      nodeIntegration: false,
-      preload: path.join(__dirname, "..", "renderer", "preloadChild.js"),
-      offscreen: true,
-    },
-  })
+	let childWindow = new BrowserWindow({
+		parent: mainWindow,
+		show: false,
+		webPreferences: {
+			nodeIntegration: false,
+			preload: path.join(__dirname, "..", "renderer", "preloadChild.js"),
+			offscreen: true,
+		},
+	})
 
-  // Child window script (crawler)
-  childWindow.webContents.on("did-finish-load", function () {
-    const windowUrl = childWindow.webContents.getURL()
-    // Checkpoint - Google.com
-    if (windowUrl.includes("google.com/webhp?as_q=&as_epq=&as_oq=&as")) {
-      childWindow.webContents.send(
-        "launch-search",
-        store.getState().current_music
-      )
-    }
+	// Child window script (crawler)
+	childWindow.webContents.on("did-finish-load", function () {
+		const windowUrl = childWindow.webContents.getURL()
+		// Checkpoint - Google.com
+		if (windowUrl === "https://www.google.com/") {
+			childWindow.webContents.send(
+				"launch-search",
+				store.getState().current_music
+			)
+		}
 
-    // Checkpoint - Google search
-    if (windowUrl.includes("google.com/search?")) {
-      childWindow.webContents.send(
-        "enter-genius",
-        store.getState().current_music
-      )
-    }
+		// Checkpoint - Google search
+		if (windowUrl.includes("google.com/search?")) {
+			childWindow.webContents.send(
+				"enter-genius",
+				store.getState().current_music
+			)
+		}
 
-    // Checkpoint - Genius.com
-    if (windowUrl.includes("genius.com")) {
-      childWindow.webContents.send(
-        "sendbackhtml",
-        store.getState().current_music
-      )
-    } else return console.log("fin")
-  })
-  return childWindow
+		// Checkpoint - Genius.com
+		if (windowUrl.includes("genius.com")) {
+			childWindow.webContents.send(
+				"sendbackhtml",
+				store.getState().current_music
+			)
+		} else return console.log("fin")
+	})
+	return childWindow
 }
--- backend/ipcListeners.js
+++ backend/ipcListeners.js
@@ -1,61 +1,59 @@
-const { ipcMain } = require('electron/main')
+const { ipcMain } = require("electron/main")
 
 module.exports = (app, mainWindow, childWindow) => {
-  const {
-    logoutFromLocalStorage,
-    setBackgroundColorFromLocalStorage,
-    setTextColorFromLocalStorage,
-  } = require('./utility')
+	const {
+		logoutFromLocalStorage,
+		setBackgroundColorFromLocalStorage,
+		setTextColorFromLocalStorage,
+	} = require("./utility")
 
-  // Handle Window controls
-  ipcMain.on('minimize-window', () => {
-    mainWindow.minimize()
-  })
-  ipcMain.on('toggle-maximize-window', () => {
-    if (mainWindow.isMaximized()) return mainWindow.unmaximize()
-    mainWindow.maximize()
-  })
-  ipcMain.on('close-window', () => {
-    app.quit()
-  })
+	// Handle Window controls
+	ipcMain.on("minimize-window", () => {
+		mainWindow.minimize()
+	})
+	ipcMain.on("toggle-maximize-window", () => {
+		if (mainWindow.isMaximized()) return mainWindow.unmaximize()
+		mainWindow.maximize()
+	})
+	ipcMain.on("close-window", () => {
+		app.quit()
+	})
 
-  // Main Window
-  ipcMain.on('logout', () => {
-    logoutFromLocalStorage()
-    store.dispatch(updateRefreshToken(null))
-    store.dispatch(updateCodeVerifier(null))
-  })
+	// Main Window
+	ipcMain.on("logout", () => {
+		logoutFromLocalStorage()
+		store.dispatch(updateRefreshToken(null))
+		store.dispatch(updateCodeVerifier(null))
+	})
 
-  ipcMain.on('set-background-color-storage', (event, arg) => {
-    setBackgroundColorFromLocalStorage(arg)
-  })
+	ipcMain.on("set-background-color-storage", (event, arg) => {
+		setBackgroundColorFromLocalStorage(arg)
+	})
 
-  ipcMain.on('set-text-color-storage', (event, arg) => {
-    setTextColorFromLocalStorage(arg)
-  })
+	ipcMain.on("set-text-color-storage", (event, arg) => {
+		setTextColorFromLocalStorage(arg)
+	})
 
-  // Child Window
-  ipcMain.on('load-url', async (event, current_music) => {
-    childWindow.loadURL(
-      'https://www.google.com/webhp?as_q=&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=&cr=countryUS&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&as_filetype=&tbs='
-    )
-    childWindow.webContents.setAudioMuted(true)
-  })
+	// Child Window
+	ipcMain.on("load-url", async (event, current_music) => {
+		childWindow.loadURL("https://www.google.com/")
+		childWindow.webContents.setAudioMuted(true)
+	})
 
-  // No Valid url from crawler - > trigger display
-  ipcMain.on('no-url-found', async (event, obj) => {
-    mainWindow.webContents.send('trigger-no-lyrics-display')
-  })
+	// No Valid url from crawler - > trigger display
+	ipcMain.on("no-url-found", async (event, obj) => {
+		mainWindow.webContents.send("trigger-no-lyrics-display")
+	})
 
-  ipcMain.on('hereishtml', (event, html) => {
-    console.log('html received -> hereishtml')
-    const removeLinks = html.replace(/(<a.*?>|<\/a>)/gm, '')
-    const addSpanBrackets1 = removeLinks.replace(
-      /\[/gm,
-      '<span class="lyrics-in-brackets">['
-    )
-    const addSpanBrackets2 = addSpanBrackets1.replace(/\]/gm, ']</span>')
+	ipcMain.on("hereishtml", (event, html) => {
+		console.log("html received -> hereishtml")
+		const removeLinks = html.replace(/(<a.*?>|<\/a>)/gm, "")
+		const addSpanBrackets1 = removeLinks.replace(
+			/\[/gm,
+			'<span class="lyrics-in-brackets">['
+		)
+		const addSpanBrackets2 = addSpanBrackets1.replace(/\]/gm, "]</span>")
 
-    mainWindow.webContents.send('reply-html', addSpanBrackets2)
-  })
+		mainWindow.webContents.send("reply-html", addSpanBrackets2)
+	})
 }
