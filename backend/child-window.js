f1ba30ccb75487aa6dc1e2663ebea374751ae9a4
NoailletasJordan <j.noailletas@gmail.com>
2022-04-24 01:10:25 +0200

    removed adds

--- backend/child-window.js
+++ backend/child-window.js
@@ -1,43 +1,43 @@
-const path = require("path")
-const { BrowserWindow } = require("electron")
+const path = require('path')
+const { BrowserWindow } = require('electron')
 
 module.exports = () => {
-	let childWindow = new BrowserWindow({
-		parent: mainWindow,
-		show: false,
-		webPreferences: {
-			nodeIntegration: false,
-			preload: path.join(__dirname, "..", "renderer", "preloadChild.js"),
-			offscreen: true,
-		},
-	})
+  let childWindow = new BrowserWindow({
+    parent: mainWindow,
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, '..', 'renderer', 'preloadChild.js'),
+      offscreen: true
+    }
+  })
 
-	// Child window script (crawler)
-	childWindow.webContents.on("did-finish-load", function () {
-		const windowUrl = childWindow.webContents.getURL()
-		// Checkpoint - Google.com
-		if (windowUrl === "https://www.google.com/") {
-			childWindow.webContents.send(
-				"launch-search",
-				store.getState().current_music
-			)
-		}
+  // Child window script (crawler)
+  childWindow.webContents.on('did-finish-load', function () {
+    const windowUrl = childWindow.webContents.getURL()
+    // Checkpoint - Google.com
+    if (windowUrl === 'https://www.google.com/') {
+      childWindow.webContents.send(
+        'launch-search',
+        store.getState().current_music
+      )
+    }
 
-		// Checkpoint - Google search
-		if (windowUrl.includes("google.com/search?")) {
-			childWindow.webContents.send(
-				"enter-genius",
-				store.getState().current_music
-			)
-		}
+    // Checkpoint - Google search
+    if (windowUrl.includes('google.com/search?')) {
+      childWindow.webContents.send(
+        'enter-genius',
+        store.getState().current_music
+      )
+    }
 
-		// Checkpoint - Genius.com
-		if (windowUrl.includes("genius.com")) {
-			childWindow.webContents.send(
-				"sendbackhtml",
-				store.getState().current_music
-			)
-		} else return console.log("fin")
-	})
-	return childWindow
+    // Checkpoint - Genius.com
+    if (windowUrl.includes('genius.com')) {
+      childWindow.webContents.send(
+        'sendbackhtml',
+        store.getState().current_music
+      )
+    } else return console.log('fin')
+  })
+  return childWindow
 }
--- renderer/preloadChild.js
+++ renderer/preloadChild.js
@@ -1,38 +1,37 @@
-const { ipcRenderer } = require('electron')
-const listOfBannedWords = require('./banned-words')
+const { ipcRenderer } = require("electron")
+const listOfBannedWords = require("./banned-words")
 
-ipcRenderer.on('launch-search', (event, args) => {
-  console.log('launch-search')
+ipcRenderer.on("launch-search", (event, args) => {
+  console.log("launch-search")
 
-  document.querySelector('.gLFyf.gsfi').value = `genius lyrics ${nameFormatter(
+  document.querySelector(".gLFyf.gsfi").value = `genius lyrics ${nameFormatter(
     args.name
   )}${args.artistsMax2}`
-  document.querySelector('.gNO89b').click()
+  document.querySelector(".gNO89b").click()
 })
 
-ipcRenderer.on('enter-genius', (event, args) => {
-  console.log('enter-genius')
+ipcRenderer.on("enter-genius", (event, args) => {
+  console.log("enter-genius")
   let secondLinkUrl
   let secondLinkNode
   let thirdLinkNode
   let thirdLinkUrl
   let fourthLinkNode
   let fourthLinkUrl
-  const linkUrl = document.querySelector('.g a').getAttribute('href')
-  const linkNode = document.querySelector('.g a')
-  const list = document.querySelectorAll('.g')
+  const linkUrl = document.querySelector(".g a").getAttribute("href")
+  const linkNode = document.querySelector(".g a")
+  const list = document.querySelectorAll(".g")
   try {
-    secondLinkNode = list[1].querySelector('a')
-    secondLinkUrl = list[1].querySelector('a').getAttribute('href')
-    thirdLinkNode = list[2].querySelector('a')
-    thirdLinkUrl = list[2].querySelector('a').getAttribute('href')
-    fourthLinkNode = list[3].querySelector('a')
-    fourthLinkUrl = list[3].querySelector('a').getAttribute('href')
+    secondLinkNode = list[1].querySelector("a")
+    secondLinkUrl = list[1].querySelector("a").getAttribute("href")
+    thirdLinkNode = list[2].querySelector("a")
+    thirdLinkUrl = list[2].querySelector("a").getAttribute("href")
+    fourthLinkNode = list[3].querySelector("a")
+    fourthLinkUrl = list[3].querySelector("a").getAttribute("href")
   } catch (error) {
     console.log(error)
   }
 
-  //linkNode.click()
   const linkSelected = linkSelector(
     args.name,
     linkUrl,
@@ -40,25 +39,31 @@ ipcRenderer.on('enter-genius', (event, args) => {
     thirdLinkUrl,
     fourthLinkUrl
   )
-  console.log('link selector number :', linkSelected)
+  console.log("link selector number :", linkSelected)
   if (linkSelected === 0) return linkNode.click()
   if (linkSelected === 1) return secondLinkNode.click()
   if (linkSelected === 2) return thirdLinkNode.click()
   if (linkSelected === 3) return fourthLinkNode.click()
-  if (linkSelected === -1) return ipcRenderer.send('no-url-found')
+  if (linkSelected === -1) return ipcRenderer.send("no-url-found")
 })
 
-ipcRenderer.on('sendbackhtml', (event, arg) => {
-  console.log('preload: received sendbackhtml')
+ipcRenderer.on("sendbackhtml", (event, arg) => {
+  console.log("preload: received sendbackhtml")
+
+  // Remove page adds
+  document
+    .querySelectorAll("div[data-exclude-from-selection]")
+    .forEach((e) => e.replaceWith(document.createElement("br")))
+
   ipcRenderer.send(
-    'hereishtml',
+    "hereishtml",
     [...document.querySelectorAll('div[data-lyrics-container="true"]')]
       .map((node) => node.innerHTML)
-      .join('<br/>')
+      .join("<br/>")
   )
 })
 
-ipcRenderer.on('mess', (e, arg) => {
+ipcRenderer.on("mess", (e, arg) => {
   console.log(arg)
 })
 
@@ -72,26 +77,26 @@ const linkSelector = (title, link, secondLink, thirdLink, fourthLink) => {
   // Check if link exist and be on genius.com and  include "lyrics"
   if (
     !link ||
-    !link.includes('https://genius.com/') ||
-    !link.includes('lyrics')
+    !link.includes("https://genius.com/") ||
+    !link.includes("lyrics")
   )
     linkBool = false
   if (
     !secondLink ||
-    !secondLink.includes('https://genius.com/') ||
-    !secondLink.includes('lyrics')
+    !secondLink.includes("https://genius.com/") ||
+    !secondLink.includes("lyrics")
   )
     secondLinkBool = false
   if (
     !thirdLink ||
-    !thirdLink.includes('https://genius.com/') ||
-    !thirdLink.includes('lyrics')
+    !thirdLink.includes("https://genius.com/") ||
+    !thirdLink.includes("lyrics")
   )
     thirdLinkBool = false
   if (
     !fourthLink ||
-    !fourthLink.includes('https://genius.com/') ||
-    !fourthLink.includes('lyrics')
+    !fourthLink.includes("https://genius.com/") ||
+    !fourthLink.includes("lyrics")
   )
     fourthLinkBool = false
 
@@ -106,18 +111,18 @@ const linkSelector = (title, link, secondLink, thirdLink, fourthLink) => {
 
   // if "remix in url but not title" - this test should be last
   if (
-    !title.toLowerCase().includes('remix') &&
-    link.toLowerCase().includes('remix') &&
-    !secondLink.toLowerCase().includes('remix') &&
+    !title.toLowerCase().includes("remix") &&
+    link.toLowerCase().includes("remix") &&
+    !secondLink.toLowerCase().includes("remix") &&
     secondLinkBool
   ) {
     linkBool = false
   }
 
-  console.log('linkbool', linkBool)
-  console.log('secondLinkBool', secondLinkBool)
-  console.log('thirdLinkBool', thirdLinkBool)
-  console.log('fourthLinkBool', fourthLinkBool)
+  console.log("linkbool", linkBool)
+  console.log("secondLinkBool", secondLinkBool)
+  console.log("thirdLinkBool", thirdLinkBool)
+  console.log("fourthLinkBool", fourthLinkBool)
 
   // return -1 : error ; 0 = link ; 1 = secondLink
   if (linkBool) return 0
@@ -140,5 +145,5 @@ function urlWithoutBannedWords(listOfBannedWords, currentLink) {
 
 function nameFormatter(name) {
   // Filter "(feat." and max 25 caracter
-  return name.replace(/(\(feat.*|\(with.* | feat\..*)/gm, '').slice(0, 25)
+  return name.replace(/(\(feat.*|\(with.* | feat\..*)/gm, "").slice(0, 25)
 }
